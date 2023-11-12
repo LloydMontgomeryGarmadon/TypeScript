13df81b08e9aecab8af7fecd1d80c951360471aa
LloydMontgomeryGarmadon <crazymathew29@gmail.com>
2023-11-12 19:35:13 +0100

    12.11.2023

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,55 @@
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+"use strict";
+var _a;
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+level = 1;
+level = 'a';
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+let user = [1, "Mosh"];
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+;
+let mySize = Size.Medium;
+console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(10000);
+let employee = {
+    id: 1,
+    name: "Mosh",
+    retire: (date) => {
+        console.log(date);
+    }
+};
+let textBox = {
+    drag: () => { },
+    resize: () => { }
+};
+let quantity = 50;
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log("Hi!");
+}
+greet(undefined);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+let log = null;
+log === null || log === void 0 ? void 0 : log('a');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,88 @@ let numbers: number[] = [1, 2, 3];
 
 // Tuples
 let user: [number, string] = [1, "Mosh"];
+
+// Enums
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+enum Size { Small = 1, Medium, Large };
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
+// Functions
+function calculateTax(income: number, taxYear = 2022): number {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+
+calculateTax(10_000)
+
+// Objects
+type Employee = {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+}
+
+let employee: Employee = {
+    id: 1, 
+    name: "Mosh",
+    retire: (date: Date) => {
+        console.log(date);
+    } 
+};
+
+// Intersection Types
+type Draggable = {
+    drag: () => void
+}
+
+type Resizable = {
+    resize: () => void
+}
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+    drag: () => {},
+    resize: () => {}
+}
+
+// Literal Types (exact, specific)
+type Quantity = 50 | 100;
+let quantity: Quantity = 50;
+
+// Nullable Types
+function greet(name: string | null | undefined) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log("Hi!");
+}
+
+greet(undefined);
+
+// Optional Chaining
+type Customer = {
+    birthday?: Date
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+    return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(1);
+// Optional property access operator
+console.log(customer?.birthday?.getFullYear());
+
+// Optional element access operator
+// if (customer !== null && customer !== undefined)
+    // customer[0]
+    // customer?.[0]
+
+// Optional call
+let log: any = null;
+log?.('a');
