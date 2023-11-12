12eed43f37beffd8cd590db1e783f67e8613e4f1
LloydMontgomeryGarmadon <crazymathew29@gmail.com>
2023-11-12 05:22:56 +0100

    12.11.2023

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
-let age: number = 20;
-if (age < 50)
-    age += 10;
-console.log(age)
+// Types
+let sales: number = 123_456_789;
+let course: string = "TypeScript";
+let is_published: boolean = true;
+
+// The any Type
+let level;
+level = 1;
+level = 'a';
+
+// Compilation error, cuz parameter implicitly has an "any" type
+// function render(document) {
+//     console.log(document);
+// }
+
+// Fix, we have to use a type - or turn off "noImplicitAny" option in tsconfig.json
+function render(document: any) {
+    console.log(document);
+}
+
+// Arrays
+let numbers: number[] = [1, 2, 3];
+
+// Tuples
+let user: [number, string] = [1, "Mosh"];
